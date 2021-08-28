28c0c968b41aaec0d567bdd9110beeba44ca4239
taisso <taissoreni@gmail.com>
2021-08-28 18:35:32 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e77d7146354cbfb408441ac9a3f06a83'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1bad531a-30a5-4e1d-9e2b-f0e16ad5d160'
+};
